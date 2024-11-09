dec23c8b369d3f167570a5257b593e44526846c9
nataliamaia <nataliaborgesm@gmail.com>
2024-11-09 16:04:25 -0300

    Definida a tela de cadastroVIEW como principal

---
+++ src/main/principal.java
@@ -0,0 +1,11 @@
+
+package main;
+
+import view.cadastroVIEW;
+
+public class principal {
+    public static void main(String args[]) {
+        cadastroVIEW tela = new cadastroVIEW();
+        tela.setVisible(true);
+    }
+}
